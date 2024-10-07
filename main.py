f61e142d41475c794ca55ab44b2b74529d3e8fd3
Seijung Kim <sjtok@Seijungs-MBP.lan>
2024-10-06 23:23:35 -0400

    added script for extract and load and requirements

---
+++ main.py
@@ -0,0 +1,10 @@
+from mylib.extract import extract
+from mylib.transform_load import load
+
+# Extract
+print("Extracting data...")
+extract()
+
+# Transform and load
+print("Transforming data...")
+load()
